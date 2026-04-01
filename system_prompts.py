90b1ec965c9763cf29e84d8b4459ccfba5e3c6d5
YUBIN-githubb <yubin5744@gmail.com>
2026-04-01 23:03:23 +0900

    system prompts

---
+++ system_prompts.py
@@ -0,0 +1,40 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from anthropic import Anthropic
+
+client = Anthropic()
+model = "claude-sonnet-4-0"
+
+def add_user_message(messages, text):
+    user_message = {"role": "user", "content": text}
+    messages.append(user_message)
+
+def add_assistant_message(messages, text):
+    assistant_message = {"role": "assistant", "content": text}
+    messages.append(assistant_message)
+    
+def chat(messages, system=None): 
+    params = {
+        "model" : model,
+        "max_tokens" : 1000,
+        "messages" : messages,
+    }
+    
+    if system:
+        params["system"] = system
+       
+    message = client.messages.create(**params)
+    return message.content[0].text
+
+messages = []
+
+system = """
+        You are a patient math tutor.
+        Do not directly answer a student's questions.
+        Guide them to a solution step by step.
+    """
+
+add_user_message(messages, "5x + 3 = 2를 어떻게 풀어?")
+answer = chat(messages)
+print(answer)
