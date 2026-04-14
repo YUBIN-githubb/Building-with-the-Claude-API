3ad314111adf4555d342a18bd3fd5d5621543da4
YUBIN-githubb <yubin5744@gmail.com>
2026-04-14 23:57:22 +0900

    stream

---
+++ response_streaming.py
@@ -0,0 +1,55 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from anthropic import Anthropic
+
+client = Anthropic()
+model = "claude-sonnet-4-0"
+
+def add_user_message(messages, text):
+    user_message = {"role": "user", "content": text}
+    messages.append(user_message)
+
+def add_assistant_message(messages, text):
+    assistant_message = {"role": "assistant", "content": text}
+    messages.append(assistant_message)
+    
+def chat(messages, system=None, temperature=1.0): 
+    params = {
+        "model" : model,
+        "max_tokens" : 1000,
+        "messages" : messages,
+        "temperature" : temperature,
+    }
+    
+    if system:
+        params["system"] = system
+       
+    message = client.messages.create(**params)
+    return message.content[0].text
+
+messages= []
+
+# add_user_message(messages, "Write a one sentence description of a fake database")
+# stream = client.messages.create(
+#     model=model,
+#     max_tokens=1000,
+#     messages=messages,
+#     stream=True
+# )
+
+# for event in stream:
+#     print(event)
+
+add_user_message(messages, "Write a one sentence description of a fake database")
+
+with client.messages.stream(
+    model=model,
+    max_tokens=1000,
+    messages=messages
+) as stream:
+    for text in stream.text_stream:
+        #print(text, end="")
+        pass
+    final_message = stream.get_final_message()
+    print(final_message)
