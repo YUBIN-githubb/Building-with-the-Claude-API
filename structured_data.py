10a77017a1455d83cfb724ebd2c3b4b7c0803d92
YUBIN-githubb <yubin5744@gmail.com>
2026-04-15 22:44:26 +0900

    structured data

---
+++ structured_data.py
@@ -0,0 +1,42 @@
+import json
+from dotenv import load_dotenv
+load_dotenv()
+
+from anthropic import Anthropic
+
+client = Anthropic()
+model = "claude-sonnet-4-0"
+
+def add_user_message(messages, text):
+    user_message = {"role": "user", "content": text}
+    messages.append(user_message)
+
+def add_assistant_message(messages, text):
+    assistant_message = {"role": "assistant", "content": text}
+    messages.append(assistant_message)
+    
+def chat(messages, system=None, temperature=1.0, stop_sequences=None): 
+    params = {
+        "model" : model,
+        "max_tokens" : 1000,
+        "messages" : messages,
+        "temperature" : temperature,
+    }
+    
+    if system:
+        params["system"] = system
+        
+    if stop_sequences:
+        params["stop_sequences"] = stop_sequences
+       
+    message = client.messages.create(**params)
+    return message.content[0].text
+
+messages= []
+
+add_user_message(messages,"Generate a very short event bridge rule as json")
+add_assistant_message(messages,"```json")
+answer = chat(messages, stop_sequences=["```"])
+clean_json = json.loads(answer.strip())
+
+print(clean_json)
